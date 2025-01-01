0983804c6626945fc5056de942bd45292a4cd244
aashish-nomad <aashish.nomad@gmail.com>
2025-01-01 13:01:57 +0545

    Retreive old form data using flash sessions

--- Core/functions.php
+++ Core/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Response;
+use Core\Session;
 
 function dd($value)
 {
@@ -46,4 +47,9 @@ function redirect($url)
 {
   header("location: {$url}");
   exit();
+}
+
+function old($key, $default = '')
+{
+  return Session::get('old')[$key] ?? $default;
 }
